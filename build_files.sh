c968e7842327ef51a62b550a203fa094a3c3ca5f
llSiddharthll <siddharthgreat443@gmail.com>
2025-07-16 02:56:14 +0530

    updated

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,6 @@
-
-pip install -r requirements.txt
-python3 manage.py collectstatic --noinput
+echo "BUILD START"
+pip install --upgrade pip
+pip3 install -r requirements.txt
+python3.9 manage.py makemigrations
+python3.9 manage.py migrate
+echo "BUILD DONE"
